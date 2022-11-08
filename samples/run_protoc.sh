ba5317379a400f218ddf31869d14d916878aa051
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-11-08 17:12:50 +0800

    添加示例代码

--- samples/run_protoc.sh
+++ samples/run_protoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=myproto --go_out=myproto-go --go_opt=paths=source_relative asset/basic_asset.proto owner/owner.proto
+protoc --proto_path=myproto --go_out=myproto-go --go_opt=paths=source_relative asset/basic_asset.proto owner/owner.proto api/show_info.proto
