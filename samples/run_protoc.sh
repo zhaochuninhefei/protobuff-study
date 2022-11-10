3eec20715f695e374b042fff7e210b33eb45e4f2
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-11-10 14:40:27 +0800

    改进编译脚本

--- samples/run_protoc.sh
+++ samples/run_protoc.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# 发生错误时立即终止脚本
+set -e
+echo "===== protoc编译开始 ====="
+# 指定proto工程目录(相对当前目录)
 proto_path=myproto
+# 指定go代码工程目录(相对当前目录)
 go_out=myproto-go
 
 # 删除目标工程下的代码文件
-if [ -d "${go_out}/api" ]; then
-  rm -rf "${go_out}/api"
-fi
-if [ -d "${go_out}/asset" ]; then
-  rm -rf "${go_out}/asset"
-fi
-if [ -d "${go_out}/owner" ]; then
-  rm -rf "${go_out}/owner"
-fi
+# 用find命令寻找`${go_out}`下的目录。
+#  `-maxdepth 1 -mindepth 1`表示只向下寻找一层。
+#  `-type d`表示只寻找目录。
+#  `\( ! -iname ".*" \)`表示排除掉隐藏目录或文件。
+# find结果借由管道传递给`while ... do`，对其结果一行一行处理。
+#  `read -r line`表示将当前行写入`line`变量，`-r`表示禁止反斜杠转义。
+find ${go_out} -maxdepth 1 -mindepth 1 -type d \( ! -iname ".*" \) | while read -r line; do
+    echo "删除: $line"
+    rm -rf "$line"
+done
 
 # 检查删除结果
+echo
 echo "----- 删除代码后:"
-ls -l ${go_out}/
+tree ${go_out}/
 
 # 编译go代码
 # shellcheck disable=SC2046
@@ -27,5 +34,9 @@ protoc \
   $(find ${proto_path} -iname "*.proto")
 
 # 检查编译结果
+echo
 echo "----- 重新编译后:"
-ls -l ${go_out}/
+tree ${go_out}/
+
+echo
+echo "===== protoc编译结束 ====="
